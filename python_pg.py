dbffc85dea60a5ec73245d913dc9bb3a4b61b4fd
Cole-C25 <ctc016@uark.edu>
2025-10-19 18:14:22 -0500

    Retool python_pg.py with cleaner code and add a function to pull the database from the SQL server.

--- python_pg.py
+++ python_pg.py
@@ -6,56 +6,49 @@ import main
 
 # Define connection parameters
 connection_params = {
-    'dbname': 'photon',
-    'user': 'student',
-    #'password': 'student',
-    #'host': 'localhost',
-    #'port': '5432'
+    'dbname': 'postgres',
+    'user': 'postgres',
+    'password': 'student',
+    'host': 'localhost',
+    'port': '5432'
 }
 
-def add(id, name):
-    try:
-        # Connect to PostgreSQL
-        conn = psycopg2.connect(**connection_params)
-        cursor = conn.cursor()
-
-        # Execute a query
-        cursor.execute("SELECT version();")
-
-        # Fetch and display the result
-        version = cursor.fetchone()
-        print(f"Connected to - {version}")
-
-
-        # Insert sample data
-        cursor.execute('''
-            INSERT INTO players (id, codename)
-            VALUES (%s, %s)
-        ''', (id, name))
-
-        # Commit the changes
-        conn.commit()
-
-        # Fetch and display data from the table
-        cursor.execute("SELECT * FROM players;")
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-
-    except Exception as error:
-        print(f"Error connecting to PostgreSQL database: {error}")
-
-def disconnect():
-    # Connect to PostgreSQL
+try:
     conn = psycopg2.connect(**connection_params)
+    # Connect to PostgreSQL
     cursor = conn.cursor()
 
-    cursor.execute("DELETE FROM players")
+    # Execute a query
+    cursor.execute("SELECT version();")
+
+    # Fetch and display the result
+    version = cursor.fetchone()
+    print(f"Connected to - {version}")
+except Exception as error:
+    print(f"Error connecting to PostgreSQL database: {error}")
+
+def add(id, name):
+    # Insert data
+    cursor.execute('''
+        INSERT INTO players (id, codename)
+        VALUES (%s, %s)
+    ''', (id, name))
+
+    # Commit the changes
     conn.commit()
 
+    # Fetch and display data from the table
+    print(f"Successfully addded to databse! Currently in table:")
     cursor.execute("SELECT * FROM players;")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
 
+def retrieve_table():
+    cursor.execute("SELECT * FROM players;")
+    return cursor.fetchall()
+
+def disconnect():
+    cursor.close()
+    conn.close()
 
